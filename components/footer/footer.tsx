f8139598a7629d3b758a5e7af1c9c0209d4c2e52
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-06-22 18:57:56 +0700

    feat(components/footer): Сверстать компонент Footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from "classnames";
+
+import styles from './footer.module.css';
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <ul className={styles.footerLinks}>
+            <li>
+                <a href="#" className={classNames(styles.footerLink, 'hover')}>Вопросы-ответы</a>
+            </li>
+            <li>
+                <a href="#" className={classNames(styles.footerLink, 'hover')}>О нас</a>
+            </li>
+        </ul>
+    </footer>
+);
+
+export default Footer;
