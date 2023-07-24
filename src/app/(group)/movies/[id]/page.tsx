15834a04a678ed1c25b85a643271d0a5dd9b060e
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-24 18:51:19 +0700

    fix: movies-id-page

--- src/app/(group)/movies/[id]/page.tsx
+++ src/app/(group)/movies/[id]/page.tsx
@@ -2,26 +2,14 @@
 
 import React from 'react';
 
-import Loading from '@/app/loading';
 import { MovieContainer } from '@/components/movie-container/movie-container';
 import { ReviewsContainer } from '@/components/reviews-container/reviews-container';
-import { useGetMovieByIdQuery } from '@/services/api/movies-api';
 
 interface IMoviePageProps {
   params: { id: string };
 }
 
 export default function MoviePage({ params }: IMoviePageProps) {
-  const { isLoading: isMovieLoading, data: movie } = useGetMovieByIdQuery(params.id);
-
-  if (isMovieLoading) {
-    return <Loading />;
-  }
-
-  if (!movie) {
-    return null;
-  }
-
   return (
     <>
       <MovieContainer movieId={params.id} />
