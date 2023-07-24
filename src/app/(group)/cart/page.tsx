7b7ce7d6fab3aa44c0ca3027999e978152c66547
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-24 18:46:52 +0700

    fix: cart-page

--- src/app/(group)/cart/page.tsx
+++ src/app/(group)/cart/page.tsx
@@ -1,9 +1,7 @@
-'use client';
-
 import React from 'react';
 
 import { CartPage } from '@/pages-components/cart-page/cart-page';
 
-export default function CardPage() {
+export default function Page() {
   return <CartPage />;
 }
