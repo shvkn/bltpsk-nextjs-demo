78507be6c1b2b1d1219fab30ed2c95eb5651f60e
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-24 19:09:19 +0700

    fix: type searchParams app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import MoviesPage from '@/pages-components/movies-page/movies-page';
 
-export default function Home({ searchParams }) {
+interface IPageParams {
+  searchParams: {
+    cinema: string;
+  };
+}
+
+export default function Page({ searchParams }: IPageParams) {
   return <MoviesPage cinemaId={searchParams.cinema} />;
 }
--- src/components/tickets-container/tickets-container.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const TicketsContainer: React.FC = () => {};
