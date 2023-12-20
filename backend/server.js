02c15bc0cbf87034ac5939efd736785cd98ed673
Anatoly Shuvakhin <shuvakhin@yandex.ru>
2023-12-20 13:56:07 +0700

    feat: integrate backend into docker

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const api = require('./api');
+const bodyParser = require('body-parser');
+const port = 3001;
+
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use(bodyParser.json());
+app.use('/api', api);
+
+app.listen(port, () => console.log('Listening at http://localhost:' + port));
